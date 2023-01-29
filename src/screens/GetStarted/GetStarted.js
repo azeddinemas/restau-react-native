0899841ddbdd31356d1b205fdb05b444d1116524
azeddine <maslouhazeddine@gmail.com>
2023-01-29 22:49:03 +0100

    add leanding page

---
+++ src/screens/GetStarted/GetStarted.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View,Image,Text } from 'react-native'
+import React from 'react'
+import Get from "../../res/images/Illustrations/il_getStarted.png";
+
+const GetStarted = () => {
+  return (
+    <View style={styles.screen}>
+      <Image source={Get} style={styles.image}/>
+      <View style={styles.wrapperSlogan}>
+          <Text style={styles.textSlogan}>Shop Your Daily</Text>
+          <Text style={styles.textSlogan}>Necessary</Text>
+      </View>
+    </View>
+  )
+}
+
+export default GetStarted;
+
+const styles = StyleSheet.create({
+    screen:{flex:1,paddingHorizontal:20, justifyContent:'center'},
+    image : {height:255,width:'100%',resizeMode:'stretch'},
+    wrapperSlogan:{marginTop:51},
+    textSlogan:{fontSize:30,color:'#0FA956',textAlign:'center',fontFamily: 'Poppins-SemiBold'}
+})
